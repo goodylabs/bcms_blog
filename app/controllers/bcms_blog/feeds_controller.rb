1b615c965f10ee9e8a42991b36694000fa5098d0
Adam Migodziński <adam.migodzinski@goodylabs.com>
2015-05-26 08:49:18 +0200

    #787 - Rollbar notifier - #65

--- app/controllers/bcms_blog/feeds_controller.rb
+++ app/controllers/bcms_blog/feeds_controller.rb
@@ -2,8 +2,11 @@ module BcmsBlog
   class FeedsController < ApplicationController
   
     def index
-      @blog = Blog.find(params[:blog_id])
-      @blog_posts = @blog.posts.published.all(:limit => 10, :order => "published_at DESC")
+      id = params[:blog_id] || params[:id]
+      unless id.blank?
+        @blog = Blog.find_by(id: id)
+        @blog_posts = @blog.posts.published.all(:limit => 10, :order => "published_at DESC") if @blog
+      end
     end
   
   end
